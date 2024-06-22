0eb5eec2b7637609c915733e45f5661286ae0fbc
koladev <onaelmangabo@gmail.com>
2024-06-22 23:33:52 +0100

    :bug: fix env var loading

--- dashboard/src/utils.ts
+++ dashboard/src/utils.ts
@@ -1,6 +1,10 @@
 import wretch from "wretch";
 import dayjs from "dayjs";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5002";
 
 const api = () => wretch(`${API_URL}/api/sendhooks/v1`);
